9bd84a2499c790da88368977f102bddeb55dfdf2
Chimmysmallz <105133066+Chimmysmallz@users.noreply.github.com>
2022-06-07 22:20:10 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,30 @@
-g
+#include <stdio.h>
+
+int main()
+{
+	//loop counter or a variable that
+	//will store initial alphabet,
+	//from where we will print the alphabets
+	char alphabet;
+	//assigning 'a' as initial alphabet
+	alphabet='a';
+
+	//print statement
+	printf("Lowercase alphabets:\n");
+
+  for I in 1 .. 10 loop
+  Put_Line ("Hello");
+end loop;
+  
+	//loop statement, that will check the condition
+	//and print the alphabets from 'a' to 'z'
+	while(alphabet<='z')
+	{
+		//printing the alphabets
+		printf("%c ",alphabet);
+		//increasing the value by 1
+		alphabet++;
+	}
+
+	return 0;
+}
