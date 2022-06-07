88fe7ea4d3ff0921b98b083a5b86f9c444b0bd74
Chimmysmallz <105133066+Chimmysmallz@users.noreply.github.com>
2022-06-07 22:09:18 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,16 @@
-
+#include < stdio.h >  
+  
+int main()  
+{  
+    char ch = 'a';  
+  
+    _putchar("Lowercase English Alphabets:\n");  
+    while(ch <= 'z')  
+    {  
+        _putchar("%c ", ch);  
+        ch++;  
+    }  
+    putc("\n");  
+  
+    return 0;  
+} 
