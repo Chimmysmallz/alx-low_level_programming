e1bdad57140dcc210a08149b382547b744ef9879
Chimmysmallz <105133066+Chimmysmallz@users.noreply.github.com>
2022-06-07 22:35:07 +0100

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,1 +1,27 @@
+// C program to check a given character is a lowercase character or not
+// without using library function
 
+#include <stdio.h>
+
+int isLowercase(char ch)
+{
+    if (ch >= 'a' && ch <= 'z')
+        return 1;
+
+    return 0;
+}
+
+int main()
+{
+    char ch;
+
+    _putchar("Enter character: ");
+    scanf("%c", &ch);
+
+    if (isLowercase(ch))
+        _putchar("Given character is a lowercase character\n");
+    else
+        _putchar("Given character is not a lowercase character\n");
+
+    return 0;
+}
