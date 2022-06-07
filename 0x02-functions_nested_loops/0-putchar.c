7d2e796813b6af0cdd9e4bce8c51008d4750576a
Chimmysmallz <105133066+Chimmysmallz@users.noreply.github.com>
2022-06-07 21:54:13 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,15 @@
-
+// Write a program that prints _putchar, followed by a new line
+  
+#include <stdio.h>
+  
+int main()
+{
+  
+    // Get the character to be written
+    char ch = '_putchar';
+  
+    // Write the Character to stdout
+    putchar(ch);
+  
+    return (0);
+}
