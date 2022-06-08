bd7b00a081c5c52b7d27c728f39ff18a5a06b013
Chimmysmallz <105133066+Chimmysmallz@users.noreply.github.com>
2022-06-08 15:42:45 +0100

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,15 @@
-#include <stdio.h>
-int main() {
-    char c;
-    printf("Enter a character: ");
-    scanf("%c", &c);
+#include "main.h"
+/**
+ * _isalpha - tests whether a character is from the English alphabet.
+ * @c: character to be checked.
+ * Return: 1 if the character is an English character.
+ * 0 if the character is not an English character.
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
 
-    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-        printf("%c is an alphabet.", c);
-    else
-        printf("%c is not an alphabet.", c);
-
-    return 0;
 }
