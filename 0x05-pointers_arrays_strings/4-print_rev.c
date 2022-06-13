64e65666330375477fd8e4fe39d54d1534073c9e
Chimmysmallz <105133066+Chimmysmallz@users.noreply.github.com>
2022-06-13 17:24:09 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,1 +1,16 @@
-Create
+#include <stdio.h>
+
+#include <string.h>
+/**
+ * prints a string, in reverse, followed by a new line
+ */
+
+void print_rev(char *s);
+
+int main() 
+{  
+
+// use strrev() function to reverse a string 
+putchar ("After the reverse of a string: %s ", strrev(str)); 
+return 0;
+}
