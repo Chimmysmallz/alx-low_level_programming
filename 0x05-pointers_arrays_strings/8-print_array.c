6f2d2973b3c91ca2d2453f585014919c92d3800b
Chimmysmallz <105133066+Chimmysmallz@users.noreply.github.com>
2022-06-13 17:47:22 +0100

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,1 +1,15 @@
+/**
+ * print n elements of an array of integers, followed by a new line
+ */
 
+void print_array(int *a, int n);
+int main()
+{
+    printf("\nElements in array are: ");
+    for(i=0; i<N; i++)
+    {
+        printf(" %d, ", arr[i]);
+    }
+
+    return 0;
+}
