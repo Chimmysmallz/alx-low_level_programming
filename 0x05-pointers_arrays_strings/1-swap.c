0ceba0226635e3cd2ec1d017ba3238fe6ced4558
Chimmysmallz <105133066+Chimmysmallz@users.noreply.github.com>
2022-06-13 18:00:03 +0100

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,23 +4,10 @@
  * @b: Second value to swap.
  */
 void swap_int(int *a, int *b) 
-int main()
 {
-    int num1,num2;
+	int temp;
 
-    printf("Enter value of num1: ");
-    scanf("%d",&num1);
-    printf("Enter value of num2: ");
-    scanf("%d",&num2);
-
-    //displaying numbers before swapping
-    printf("Before Swapping: num1 is: %d, num2 is: %d\n",num1,num2);
-
-    //calling the user defined function swap()
-    swap(&num1,&num2);
-
-    //displaying numbers after swapping
-    printf("After  Swapping: num1 is: %d, num2 is: %d\n",num1,num2);
-
-    return 0;
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
