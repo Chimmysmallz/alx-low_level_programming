61e9742c7b61f588c1cb0fd28fe6ac874a4d86c8
Chimmysmallz <105133066+Chimmysmallz@users.noreply.github.com>
2022-06-13 17:27:10 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,1 +1,23 @@
-Create
+/*
+* C Program to reverse a string
+*/
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+ 
+void rev_string(char *s);
+
+int main()
+{
+   char inputArray[100];
+  
+   printf("Enter a string to reverse\n");
+   gets(inputArray);
+   /*
+    * strrev() function reverses a given string
+    */
+   strrev(inputArray);
+   printf("Reversed string is: %s\n", inputArray);
+   getch();
+   return 0;
+}
