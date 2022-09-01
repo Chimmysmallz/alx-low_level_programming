c1d953cacaa44fa0d182e4dcd62685018cfd4979
Chimmysmallz <105133066+Chimmysmallz@users.noreply.github.com>
2022-09-01 15:38:53 +0100

    Update 1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,15 +1,14 @@
 #include "lists.h"
-
 /**
- * dlistint_len - to find the length of the linked list
- * @h: pointer to the head of the list
- * Return: the number
- */
+  * dlistint_len - Gets length of the list
+  * @h: The head of the list
+  * Return: Number of items in the linked list
+  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t a;
+	size_t i;
 
-	for (a = 0; h != NULL; a++)
+	for (i = 0; h; i++)
 		h = h->next;
-
-	return (a);
+	return (i);
+}
