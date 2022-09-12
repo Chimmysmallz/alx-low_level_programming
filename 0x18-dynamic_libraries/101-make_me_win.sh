2f03651614e3b95a2c69522efb4213a432b14a90
Chimmysmallz <105133066+Chimmysmallz@users.noreply.github.com>
2022-09-12 09:57:37 +0100

    Create 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
- wget -q -O /tmp/librandy1.so https://github.com/Chimmysmallz/BatchScripts/raw/master/librandy1.so
- export LD_PRELOAD=/tmp/librandy1.so
+wget -q -O /tmp/libtest.so https://github.com/Chimmysmallz/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/libtest.so
+export LD_PRELOAD=/tmp/libtest.so
