3fbbcf6af8c53218426d11ab3b956216540c9436
Chimmysmallz <105133066+Chimmysmallz@users.noreply.github.com>
2022-09-12 10:49:17 +0100

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O /tmp/libtest.so https://github.com/Chimmysmallz/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/libtest.so
-export LD_PRELOAD=/tmp/libtest.so
+wget -P /tmp/ https://raw.github.com/luischaparroc/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
