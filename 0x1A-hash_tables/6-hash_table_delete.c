4c2890cdff5b0083b0107cea9c130236a7d720e4
Chimmysmallz <105133066+Chimmysmallz@users.noreply.github.com>
2022-10-18 10:00:42 +0100

    Update 6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,32 +1,37 @@
 #include "hash_tables.h"
-
 /**
- * hash_table_delete - function to delete hash table
- * @ht: hash table to be deleted
- * Return: void
- */
+  * hash_table_delete - delete the whole hash table
+  * @ht: the hash table
+  */
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t *curr = NULL;
-	unsigned long int i = 0;
+	unsigned long int i;
+	hash_node_t *tmp;
 
 	if (ht == NULL)
 		return;
-
 	for (i = 0; i < ht->size; i++)
 	{
 		if (ht->array[i] != NULL)
 		{
+			printf("First for loop\n");
 			while (ht->array[i])
 			{
-				curr = ht->array[i];
-				free(curr->key);
-				free(curr->value);
+				printf("Freeing\n");
+				tmp = ht->array[i];
+				free(tmp->key);
+				tmp->key = NULL;
+				free(tmp->value);
+				tmp->value = NULL;
 				ht->array[i] = ht->array[i]->next;
-				free(curr);
+				free(tmp);
+				tmp = NULL;
 			}
 		}
 	}
 	free(ht->array);
+	ht->array = NULL;
 	free(ht);
+	ht = NULL;
+	printf("Leaving\n");
 }
